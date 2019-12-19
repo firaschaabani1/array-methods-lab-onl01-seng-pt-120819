e83c4ac1424547b0c7dd95123585b153db28fa82
Firas Chaabani <firaschaabani1@gmail.com>
2019-12-19 00:41:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,14 +1,16 @@
 def using_include(array, element)
-if using_include include (array, element)
+ array.include? (element)
   return "true"
 end
 
 def using_sort(array)
-
+array.sort(last)
+return "wow"
 end
 
 def using_reverse(array)
-
+array.reverse(first)
+return "arrays!"
 end
 
 def using_first(array)
