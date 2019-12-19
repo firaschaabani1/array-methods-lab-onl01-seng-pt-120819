dd0271399f3408b27a3d83b40f7e1a3d745977aa
Firas Chaabani <firaschaabani1@gmail.com>
2019-12-19 00:46:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,25 +5,20 @@ end
 
 def using_sort(array)
 array.sort(last)
-return "wow"
 end
 
 def using_reverse(array)
 array.reverse(first)
-return "arrays!"
 end
 
 def using_first(array)
 array.first(element)
-return"wow"
 end
 
 def using_last(array)
 array.last (array)
-return "arrays!"
 end
 
 def using_size(array)
 array.size (array)
-return "6"
 end
